b45685dbbf8a9f7df6e8f1d6448426c6948367bf
greedyhippy <greedyhippy@gmail.comm>
2025-07-03 17:12:49 +0100

    Fix GraphQL Playground configuration for Railway deployment

--- server.js
+++ server.js
@@ -254,12 +254,30 @@ function generateSampleProducts() {
 async function startServer() {
   const app = express();
   
+  // Enable CORS for all routes
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });
+  
   // Create Apollo Server
   const server = new ApolloServer({
     typeDefs,
     resolvers,
     introspection: true,
-    playground: true
+    playground: {
+      settings: {
+        'editor.theme': 'dark',
+        'editor.fontSize': 14,
+        'editor.fontFamily': 'Source Code Pro'
+      }
+    }
   });
 
   await server.start();
